9431a7f9e60187f6ed8312dff54cd7812dffae1b
raphaelauv <raphaelauv@users.noreply.github.com>
2017-04-11 01:43:12 +0200

    Write implementation 60%

--- conduct.c
+++ conduct.c
@@ -2,6 +2,12 @@
 
 #define FILE_mode 0666
 
+#define mode_ANONYMOUS 1
+#define mode_NAMED 2
+
+#define FLAG_CLEAN_DESTROY 1
+#define FLAG_CLEAN_CLOSE 2
+
 typedef struct custom_Mutex {
 	int var;
 	pthread_mutex_t mutex;
@@ -9,7 +15,12 @@ typedef struct custom_Mutex {
 } custom_Mutex;
 
 struct content {
+	char isEmpty;
+	size_t start;
+	size_t end;
+	size_t max;
 	custom_Mutex * cmutex;
+	char * buffCircular;
 };
 
 int init_custom_Mutex(custom_Mutex * arg) {
@@ -34,19 +45,29 @@ void clean_custom_Mutex(custom_Mutex * arg) {
 
 void clean_Content(struct content * cont) {
 	if (cont != NULL) {
+		clean_custom_Mutex(cont->cmutex);
 		free(cont);
 		cont = NULL;
 	}
-
 }
 
-void clean_Conduct(struct conduct * cond) {
+void clean_Conduct(struct conduct * cond,int flag) {
 	if (cond != NULL) {
+
+
+		if (cond->mmap != MAP_FAILED) {
+			if(msync(cond->mmap,cond->c,MS_SYNC)){
+				printf("ERROR msync()\n");
+			}
+
+			if(munmap(cond->mmap, cond->c)){
+				printf("ERROR munmap()\n");
+			}
+		}
+
+
 		if (cond->fd != -1) {
 			close(cond->fd);
-		}
-		if (cond->mmap == (caddr_t) (-1)) {
-			munmap(cond->mmap, cond->c);
 		}
 
 		free(cond);
@@ -66,21 +87,54 @@ struct conduct *conduct_create(const char *name, size_t a, size_t c) {
 
 	cond->c = c;
 	cond->a = a;
+	cond->fd = -1;
 
-	if (name == NULL) {
+	if (name != NULL) {
 
-		cond->fd = shm_open("partage", O_CREAT | O_RDWR, FILE_mode);
+		cond->modeMMAP = mode_ANONYMOUS;
+		cond->fd = shm_open(name, O_CREAT | O_RDWR | O_TRUNC, FILE_mode);
+
 		if (cond->fd < 0) {
 			goto cleanup;
 		}
 
-		cond->mmap = mmap((caddr_t) 0, cond->c, PROT_WRITE | PROT_READ,
-				MAP_SHARED, cond->fd, 0);
-		if (cond->mmap == (caddr_t) (-1)) {
+		struct stat sb;
+		off_t offset, pa_offset;
+
+		if (fstat(cond->fd, &sb) == -1){
+			goto cleanup;
+		}
+
+		offset = cond->c;
+		pa_offset = offset & ~(sysconf(_SC_PAGE_SIZE) - 1);
+
+		if (offset >= sb.st_size) {
+			if (ftruncate(cond->fd, c)) {
+				goto cleanup;
+			}
+			//TODO
+			close(cond->fd);
+			cond->fd = shm_open(name, O_CREAT | O_RDWR | O_TRUNC, FILE_mode);
+			if (cond->fd < 0) {
+				goto cleanup;
+			}
+		}
+
+		cond->mmap = mmap(NULL, cond->c /*TODO*/ , PROT_WRITE | PROT_READ,MAP_SHARED, cond->fd, 0);
+
+		if (cond->mmap == MAP_FAILED) {
 			goto cleanup;
 		}
 
 	} else {
+
+		cond->modeMMAP = mode_NAMED;
+
+		cond->mmap = mmap( NULL, cond->c , PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+
+		if (cond->mmap == MAP_FAILED) {
+			goto cleanup;
+		}
 
 	}
 
@@ -92,15 +146,18 @@ struct conduct *conduct_create(const char *name, size_t a, size_t c) {
 	}
 	init_custom_Mutex(cont->cmutex);
 
+	pthread_mutex_lock(&cont->cmutex->mutex);
+	cont->isEmpty=1;
+	pthread_mutex_unlock(&cont->cmutex->mutex);
+
 	//TODO
 
-	cleanup:
-		clean_Conduct(cond);
-		clean_Content(cont);
-
-	return NULL;
-
 	return cond;
+
+	cleanup:
+		clean_Conduct(cond,FLAG_CLEAN_DESTROY);
+		clean_Content(cont);
+		return NULL;
 
 }
 struct conduct *conduct_open(const char *name) {
@@ -121,11 +178,89 @@ ssize_t conduct_read(struct conduct *c, void *buf, size_t count) {
 		return -1;
 	}
 
-	ssize_t  reallyRead;
+	char * localBuf =(char *)buf;
 
-	//TODO
+	struct content * ct=(struct content *)c->mmap;
 
-	return  reallyRead;
+	pthread_mutex_lock(&ct->cmutex->mutex);
+
+	while (ct->isEmpty) {
+
+		if (ct->buffCircular[ct->start] == EOF) {
+			pthread_mutex_unlock(&ct->cmutex->mutex);
+			return 0;
+		} else {
+			pthread_cond_wait(&ct->cmutex->condition, &ct->cmutex->mutex);
+			continue;
+		}
+
+	}
+	size_t sizeAvailable;
+	ssize_t sizeReallyRead = 0;
+
+	char passByMiddle=0;
+
+	if (ct->start < ct->end) {
+		sizeAvailable = ct->end - ct->start;
+
+	} else {
+
+		if (ct->start == ct->end) {
+			if(ct->end!=ct->max){
+				passByMiddle=1;
+			}
+			sizeAvailable = ct->max;
+		}else{
+			passByMiddle=1;
+			sizeAvailable = ct->max - ct->start + ct->end;
+		}
+	}
+
+
+	size_t sizeToRead;
+	if (count < sizeAvailable) {
+		sizeToRead = count;
+	} else {
+		sizeToRead = sizeAvailable;
+	}
+
+	char tmp;
+	size_t firstMaxFor;
+	size_t secondMaxFor;
+	if(passByMiddle && sizeToRead> ct->max - ct->start){
+		firstMaxFor=ct->max;
+	}else{
+		firstMaxFor=ct->start + sizeToRead;
+	}
+
+
+	for (size_t i = ct->start; i < firstMaxFor; i++) {
+		if ((tmp = ct->buffCircular[ct->start + i]) == EOF) {
+
+		} else {
+			localBuf[i] = ct->buffCircular[(ct->start) + i];
+			sizeReallyRead++;
+			ct->start++;
+		}
+	}
+
+	if(passByMiddle){
+		ct->start=0;
+		for (size_t i = 0; i < secondMaxFor; i++) {
+			if ((tmp = ct->buffCircular[ct->start + i]) == EOF) {
+
+			} else {
+				localBuf[i] = ct->buffCircular[(ct->start) + i];
+				sizeReallyRead++;
+				ct->start++;
+			}
+		}
+	}
+
+
+	pthread_mutex_unlock(&ct->cmutex->mutex);
+	return sizeReallyRead;
+
 
 }
 ssize_t conduct_write(struct conduct *c, const void *buf, size_t count) {
@@ -151,15 +286,13 @@ int conduct_write_eof(struct conduct *c) {
 
 }
 void conduct_close(struct conduct *conduct) {
-	if (conduct == NULL) {
-		return;
-	}
+	clean_Conduct(conduct,FLAG_CLEAN_CLOSE);
 
 	//TODO
 
 }
 void conduct_destroy(struct conduct *conduct) {
-	clean_Conduct(conduct);
+	clean_Conduct(conduct,FLAG_CLEAN_DESTROY);
 	//TODO
 }
 
--- conduct.h
+++ conduct.h
@@ -1,41 +1,32 @@
 #ifndef CONDUCT_H_
 #define CONDUCT_H
 
-#include <stdlib.h>
+#include <dirent.h>
 #include <errno.h>
-
+#include <string.h>
+#include <fcntl.h>
+#include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <math.h>
-#include <dirent.h>
-#include <string.h>
-#include <fcntl.h> // for open
 #include <sys/file.h>
-#include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/mman.h>
 #include <semaphore.h>
-#include <sys/stat.h> /* For mode constants */
-#include <fcntl.h> /* For O_* constants */
-
-#include <pthread.h>
-
+#include <time.h>
+#include <unistd.h>
 
 #define MALERROR
 
 struct content;
 
 struct conduct{
+	char modeMMAP;
 	size_t c;
 	size_t a;
 	int fd;
 	void * mmap;
-	void *content;
 };
 
 struct conduct *conduct_create(const char *name, size_t a, size_t c);
